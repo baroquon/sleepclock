224356d1553fd7c82e488fbe68d8b83c5c07b8e5
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-10-09 09:07:54 -0500

    updated the time check

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,13 @@
+/* global moment */
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
   time: Ember.computed('clock.second', function() {
     return moment().format('h:m:s');
   }),
-  dayTime: Ember.computed('clock.hour', function(){
+  dayTime: Ember.computed('clock.second', function(){
     if(moment().format('A')==='AM'){
-      return moment().format('h') > 8;
+      return moment().format('h') >= 8;
     } else {
       return moment().format('h') === 12 || moment().format('h') < 10 ;
     }
