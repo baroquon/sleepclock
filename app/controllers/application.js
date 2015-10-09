04dd3f7e9adb85e29625e180583dd122e941bf7f
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-10-08 22:02:57 -0500

    initial commit of kids clock

---
+++ app/controllers/application.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  time: Ember.computed('clock.second', function() {
+    return moment().format('h:m:s');
+  }),
+  dayTime: Ember.computed('clock.hour', function(){
+    if(moment().format('A')==='AM'){
+      return moment().format('h') > 8;
+    } else {
+      return moment().format('h') === 12 || moment().format('h') < 10 ;
+    }
+  })
+});
